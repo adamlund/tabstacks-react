48b9d2b1425e8b03a3e9f4cc27af385762081377
Adam Lund <lundmn@gmail.com>
2024-03-11 12:38:58 -0500

    Export types from store reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chromeWindowReducer from './chromeWindowSlice';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     chromeWindows: chromeWindowReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
